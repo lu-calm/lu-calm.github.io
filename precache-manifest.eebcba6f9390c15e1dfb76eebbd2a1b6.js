0268132b2356b1de87cb0f8a19a863b6223da136
lu-calm <lucalm@tutanota.com>
2020-08-06 16:54:20 +1000

    Updates

---
+++ precache-manifest.eebcba6f9390c15e1dfb76eebbd2a1b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371d2f950a0d062f5978bc89622bb567",
+    "url": "/eth-ter-i-die/index.html"
+  },
+  {
+    "revision": "e9a875984d409a1a6ac2",
+    "url": "/eth-ter-i-die/static/css/2.7ef9b196.chunk.css"
+  },
+  {
+    "revision": "7237162a4033173cee93",
+    "url": "/eth-ter-i-die/static/css/main.e214a1e2.chunk.css"
+  },
+  {
+    "revision": "e9a875984d409a1a6ac2",
+    "url": "/eth-ter-i-die/static/js/2.c7a134ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/eth-ter-i-die/static/js/2.c7a134ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7237162a4033173cee93",
+    "url": "/eth-ter-i-die/static/js/main.3caee536.chunk.js"
+  },
+  {
+    "revision": "69529377e196ba9314e8",
+    "url": "/eth-ter-i-die/static/js/runtime-main.95ab4904.js"
+  }
+]);
