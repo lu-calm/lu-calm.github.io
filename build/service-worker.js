fcc62587459415755daaf947ed6a205369205ba2
lu-calm <lucalm@tutanota.com>
2020-08-19 16:38:47 +1000

    Even more content

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lu-calm.github.io/precache-manifest.7ecea90c298f25f4127b7cc421779cb6.js"
+  "/lu-calm.github.io/precache-manifest.552bc64ff617d0b6f6825dc2e4787da4.js"
 );
 
 self.addEventListener('message', (event) => {
